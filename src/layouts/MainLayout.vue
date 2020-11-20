615fd792bace5aba2d1c242fc42905092cdcabc0
Samuelikz <62406705+samuelikz@users.noreply.github.com>
2020-11-20 17:40:56 -0300

    Iniciar

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <q-layout view="lhr lpR lfr">
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
